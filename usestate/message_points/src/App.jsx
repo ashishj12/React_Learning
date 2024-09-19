dda5efff1e00c7e6e4f00690b726f3fc86cef5a8
ashishj12 <jangidashish38@gmail.com>
2024-09-19 17:44:15 +0530

    add message in bullet points

---
+++ usestate/message_points/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={message}
+          placeholder="Enter a message"
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <input
+          type="button"
+          value="Add"
+          onClick={(e) => {
+            setMessageList([
+              ...messageList,
+              {
+                id: messageList.length + 1,
+                message: message,
+              },
+            ]);
+            setMessage(""); 
+          }}
+        />
+        <ul>
+          {messageList.map((m) => (
+            <li key={m.id}>{m.message}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
