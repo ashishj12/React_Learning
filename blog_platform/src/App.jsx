2d6417e27ce7ddf4f21c09ebed1479287fa24c12
ashishj12 <jangidashish38@gmail.com>
2024-10-15 20:08:30 +0530

    setup enviroment variables

---
+++ blog_platform/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URI);
+  return (
+    <>
+      <h1>Blog Platform</h1>
+    </>
+  );
+}
+
+export default App;
